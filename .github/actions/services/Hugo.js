da233f2c11376ca1d5c70350906b6393fd049932
Floren Munteanu <19806136+fmunteanu@users.noreply.github.com>
2025-07-07 03:16:07 -0400

    fix: update modules

--- .github/actions/services/Hugo.js
+++ .github/actions/services/Hugo.js
@@ -64,12 +64,10 @@ class HugoService extends Action {
     return this.execute('update modules', async () => {
       this.logger.info('Updating modules...');
       await this.shellService.execute('hugo', ['mod', 'clean', '--all'], { output: true, silent: false });
+      await this.shellService.execute('hugo', ['mod', 'get', '-u', './...'], { output: true, silent: false });
       const modules = this.config.get('workflow.hugo.modules');
       const sites = this.config.get('workflow.hugo.sites');
       const allDirs = [...modules, ...sites];
-      await Promise.all(allDirs.map(dir =>
-        this.shellService.execute('hugo', ['mod', 'get', '-s', dir], { output: true, silent: false })
-      ));
       await Promise.all(allDirs.map(dir =>
         this.shellService.execute('hugo', ['mod', 'tidy', '-s', dir], { output: true, silent: false })
       ));
