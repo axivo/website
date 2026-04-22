9fb124d23162a3aa831bb759784fb06feaf24bb1
Floren Munteanu <19806136+fmunteanu@users.noreply.github.com>
2026-04-22 16:25:53 -0400

    fix: projects menu

--- src/content/_meta.js
+++ src/content/_meta.js
@@ -11,5 +11,19 @@ export default {
   blog: {
     title: 'Blog',
     type: 'page'
+  },
+  projects: {
+    items: {
+      claude: {
+        title: 'Claude Collaboration Platform',
+        href: '/claude'
+      },
+      k3sCluster: {
+        title: 'K3s Cluster',
+        href: '/k3s-cluster'
+      }
+    },
+    title: 'Projects',
+    type: 'menu'
   }
 }
