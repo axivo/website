a310d1dc727be35de2b1d59fd353d8f9ff3c4b30
Floren Munteanu <19806136+fmunteanu@users.noreply.github.com>
2026-04-28 23:14:45 -0400

    fix: circular imports

--- next.config.mjs
+++ next.config.mjs
@@ -7,9 +7,9 @@
 
 import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare'
 import nextra from 'nextra'
-import { meta as claudeMeta } from '@axivo/website/claude'
-import { meta as clusterMeta } from '@axivo/website/k3s-cluster'
-import { meta as globalMeta } from '@axivo/website/global'
+import { meta as claudeMeta } from './src/config/variables/claude.js'
+import { meta as clusterMeta } from './src/config/variables/cluster.js'
+import { meta as globalMeta } from './src/config/variables/global.js'
 
 initOpenNextCloudflareForDev()
 
